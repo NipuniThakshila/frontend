c77a5912c75555eb68a6fa7b72c88c43394ad0dd
NipuniThakshila <156992147+NipuniThakshila@users.noreply.github.com>
2024-05-02 23:56:13 +0530

    first commit

---
+++ at-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import NavBar from './components/navbar/NavBar'
+import './App.css'
+import Hero from './components/Home/Hero/Hero'
+import Frame498 from './components/Frame 498/Frame498'
+import Frame499 from './components/Frame499/Frame499'
+import FAQS from './components/FAQS/FAQS'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <reactFragment>
+        <NavBar/>
+      </reactFragment>
+      <Hero/>
+      <Frame498/>
+      <Frame499/>
+      <FAQS/>
+      <Footer/>
+      
+
+    
+    </div>
+  )
+}
+
+export default App
